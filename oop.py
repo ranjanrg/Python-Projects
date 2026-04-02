f2599846f25dff1dc12ee382ae6f9c1d1e298e26
Ranjan <ranjanrg.dev@gmail.com>
2026-04-02 12:21:01 +0530

    Methods, Properties

--- oop.py
+++ oop.py
@@ -92,3 +92,72 @@ Person.lastname = "Refsnes"
 
 print(p1.lastname)
 print(p2.lastname)
+
+#modifying object property in methods of class
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    self.age += 1
+    return f"Congrats {self.name} is {self.age} year old"
+  
+p10 = Person('Ranjan', 21)
+print(p10.greet())
+print(p10.greet())
+
+#__str__()
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} is {self.age} year old"
+
+p11 = Person("Emil", 36)
+print(p11)
+
+#multiple methods
+
+class Playlist:
+  def __init__(self):
+    self.songs = []
+
+  def add_song(self,song):
+    self.songs.append(song)
+    print(f"{song} is added to your playlist")
+
+  def remove_song(self, song):
+    if song in self.songs:
+      self.songs.remove(song)
+    print(f"{song} is removed from the Playlist")
+
+  def show_songs(self):
+    for song in self.songs:
+      print(song)
+      
+
+my_playlist = Playlist()
+my_playlist.add_song("Money")
+my_playlist.add_song("Sugar")
+my_playlist.show_songs()
+my_playlist.remove_song("Money")
+my_playlist.show_songs()
+
+#delete method
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello!")
+
+p1 = Person("Emil")
+
+del p1.greet
+
+# p1.greet() # This will cause an error
+
+
